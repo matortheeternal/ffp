920782136c3bea397af5ddc27a36284f41c1b081
Colin <why101@msn.com>
2018-07-19 11:25:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ffp
+File format parser which makes it easy to parse binary file formats through data schemas. 
---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+
+let log = console.log;
+
+let dataFormats = {};
+
+let dataTypes = {
+    uint8: {
+        read: stream => stream.read(1).readUInt8()
+    },
+    array: {
+        read: (stream, entity) => {
+            let entryType = getDataType(entity.entry.type),
+                countType = getDataType(entity.count.type);
+            if (!entryType)
+                throw new Error(`Data type ${entity.entry.type} not found.`);
+            if (!countType)
+                throw new Error(`Data type ${entity.count.type} not found.`);
+            let count = countType.read(stream, entity),
+                entries = [];
+            for (let i = 0; i < count; i++)
+                entries.push(entryType.read(stream, entity));
+            return entries;
+        }
+    },
+    record: {
+        read: (stream, entity) => {
+            let format = getDataFormat(entity.format);
+            if (!format)
+                throw new Error(`Data format ${entity.format} not found.`);
+            return parseSchema(stream, format, {})
+        }
+    },
+    union: {
+        read: (stream, entity, store) => {
+            return entity.decide(stream, entity, store);
+        }
+    }
+};
+
+let parseEntity = function(stream, entity, store) {
+    let dataType = getDataType(entity.type),
+        value = dataType.read(stream, entity, store);
+    if (entity.storageKey) store[entity.storageKey] = value;
+    if (entity.callback) entity.callback(value);
+};
+
+let parseSchema = function(stream, schema, store, key) {
+    if (schema.constructor === Array) {
+        if (key) store = store[key] = {};
+        schema.forEach(entity => parseEntity(stream, entity, store));
+    } else {
+        if (key && !schema.storageKey) schema.storageKey = key;
+        parseEntity(stream, schema, store);
+    }
+    return store;
+};
+
+let parseFile = function(filePath, schema, store) {
+    let stream = fs.createReadStream(filePath);
+    stream.on('readable', () => {
+        Object.keys(schema).forEach(key => {
+            log(`Parsing ${key}`);
+            parseSchema(stream, schema[key], store, key);
+        });
+        let buf = stream.read();
+        log(!buf ? 'File parsing completed, no leftover data.' :
+            `File parsing completed, ${buf.length} unparsed bytes`);
+    });
+};
+
+let addDataType = (name, reader) => dataTypes[name] = reader;
+let getDataType = name => dataTypes[name];
+let addDataFormat = (name, format) => dataFormats[name] = format;
+let getDataFormat = name => dataFormats[name];
+let setLogCallback = fn => log = fn;
+
+module.exports = {
+    parseFile, parseSchema, parseEntity,
+    addDataType, getDataType,
+    addDataFormat, getDataFormat,
+    setLogCallback
+};
