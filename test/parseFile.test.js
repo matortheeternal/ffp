b7e0e6dfe8d16f4be1c9f0ce6b5418f9568e7135
Colin <why101@msn.com>
2018-07-20 10:02:44 -0700

    added parseFile test

---
+++ test/parseFile.test.js
@@ -0,0 +1,93 @@
+let {parseFile, addDataType, getDataFormat, addDataFormat} = require('../index'),
+    path = require('path');
+
+let iconPath = path.resolve(__dirname, './fixtures/icon.ico');
+
+describe('Parsing Files', () => {
+    const icoMagic = 0x00000100;
+
+    beforeAll(() => {
+        addDataType('uint32', {
+            read: stream => stream.read(4).readUInt32BE()
+        });
+
+        addDataType('uint32le', {
+            read: stream => stream.read(4).readUInt32LE()
+        });
+
+        addDataType('uint16le', {
+            read: stream => stream.read(2).readUInt16LE()
+        });
+
+        const PNG_HEADER = Buffer.from([137, 80, 78, 71, 13, 10, 26, 10]);
+
+        addDataType('image blobs', {
+            read: (stream, entity, store) => {
+                store.images.forEach(image => {
+                    //console.log('Parsing image ', image.len_img);
+                    image.img = stream.read(image.len_img);
+                    image.header = image.img.slice(0, 8);
+                    image.is_png = image.header.equals(PNG_HEADER);
+                });
+            }
+        });
+
+        addDataFormat('IconDirEntry', [{
+            type: 'uint8',
+            storageKey: 'width'
+        }, {
+            type: 'uint8',
+            storageKey: 'height'
+        }, {
+            type: 'uint8',
+            storageKey: 'num_colors'
+        }, {
+            type: 'uint8',
+            storageKey: 'reserved'
+        }, {
+            type: 'uint16le',
+            storageKey: 'num_planes'
+        }, {
+            type: 'uint16le',
+            storageKey: 'bpp'
+        }, {
+            type: 'uint32le',
+            storageKey: 'len_img'
+        }, {
+            type: 'uint32le',
+            storageKey: 'ofs_img'
+        }]);
+
+        addDataFormat('ico', {
+            magic: {
+                type: 'uint32',
+                expectedValue: icoMagic,
+                errorMessage: 'ICO magic does not match.'
+            },
+            images: {
+                type: 'array',
+                count: {type: 'uint16le'},
+                entry: {type: 'record', format: 'IconDirEntry'}
+            },
+            imageData: {
+                type: 'image blobs',
+                storageKey: null
+            }
+        });
+    });
+
+    it('should parse icon files', done => {
+        let icoFormat = getDataFormat('ico');
+        parseFile(iconPath, icoFormat, iconFile => {
+            expect(iconFile.magic).toBe(icoMagic);
+            expect(iconFile.images).toBeDefined();
+            expect(iconFile.imageData).toBeUndefined();
+            expect(iconFile.images.constructor).toBe(Array);
+            expect(iconFile.images.length).toBe(9);
+            expect(iconFile.images[0].img).toBeDefined();
+            expect(iconFile.images[0].header).toBeDefined();
+            expect(iconFile.images[0].is_png).toBeDefined();
+            done();
+        });
+    });
+});
